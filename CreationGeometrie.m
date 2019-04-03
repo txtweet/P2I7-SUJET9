ea7fe173ff8fc656d68f36d9069c5c662e754564
txtweet <39955857+txtweet@users.noreply.github.com>
2019-04-03 09:31:36 +0200

    Variables2

--- CreationGeometrie.m
+++ CreationGeometrie.m
@@ -1,9 +1,14 @@
 %%%%%%%%%%% P2I7 - SUJET 9 %%%%%%%%%%
 %%% Création de la géométrie du problème %%%
 
-%%% Variables du probleme
+%% Variables du probleme
 resolution = 10;    %nombre de neuds par centimètre
 hauteurDalle = 20;  %hauteur de la dalle (en cm)
 largeurDalle = 25;  %largeur de la dalle (en cm)
 rayonConduite = 1;  %rayon de l'element chauffant 
 hauteurConduite = hauteurDalle/2 % position du centre de l'élément chauffant par rapport au bas de la céllule mdoélisée
+
+
+%% Initialisation des paramètres
+neuds
+matCellule=zeros(
