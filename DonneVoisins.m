1093d38e5b6fedaf85a7813e9db4301231cb68de
Vincent Beatrix <vincent.beatrix.9@gmail.com>
2019-05-07 10:06:18 +0200

    Update DonneVoisins.m
    Commentaires sur les variables

--- DonneVoisins.m
+++ DonneVoisins.m
@@ -1,25 +1,25 @@
-% Fonction donne voisins
+%%%%%%%%%%% P2I7 - SUJET 9 %%%%%%%%%%
+%%% Fonction DonneVoisins %%%
 % Dit quels voisins sont des fluides
 % i coord en horizontal
 % j coord en vertical
-% nb1 voisin de gacuhe
-% nb2 voisin du haut
-% nb3 voisin du bas
-% nb4 voisin de droite
+
+
+
 % 0 si solide
 % 1 si fluide
 % 2 si OutOfBounds
 
 function [nb1,nb2,nb3,nb4] = DonneVoisins(i,j,matCellule)
 [M,N]=size(matCellule);
-nb1=2;
+nb1=2;  % nb1 voisin de gauche
 if((j-1)>=1 && i>=1 && i<=M && j<=N)
     nb1=0;
     if(matCellule(i,j-1)==1)
         nb1=1;
     end
 end
-nb2=2;
+nb2=2;  % nb2 voisin du haut
 if((i-1)>=1 && j>=1 && i<=M && j<=N)
     nb2=0;
     if(matCellule(i-1,j)==1)
@@ -27,14 +27,14 @@ if((i-1)>=1 && j>=1 && i<=M && j<=N)
     end
 end
 
-nb3=2;
+nb3=2;  % nb3 voisin du bas
 if((i+1)<=M && i>=1 && j>=1 && j<=N)
     nb3=0;
     if(matCellule(i+1,j)==1)
         nb3=1;
     end
 end
-nb4=2;
+nb4=2;  % nb4 voisin de droite
 if((j+1)<=N && i>=1 && j>=1 && i<=M)
     nb4=0;
     if(matCellule(i,j+1)==1)
