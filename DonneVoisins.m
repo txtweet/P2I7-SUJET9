b68423c5c9e8a2f5d899d20e1b6f013c27893ea5
txtweet <39955857+txtweet@users.noreply.github.com>
2019-05-13 17:38:29 +0200

    Update DonneVoisins.m

--- DonneVoisins.m
+++ DonneVoisins.m
@@ -15,28 +15,28 @@ function D = DonneVoisins(i,j,matCellule)
 nb1=2;
 if((j-1)>=1 && i>=1 && i<=M && j<=N)
     nb1=0;
-    if(matCellule(i,j-1)==1)
+    if(matCellule(i-1,j)==1)
         nb1=1;
     end
 end
 nb2=2;
 if((i-1)>=1 && j>=1 && i<=M && j<=N)
     nb2=0;
-    if(matCellule(i-1,j)==1)
+    if(matCellule(i,j-1)==1)
         nb2=1;
     end
 end
 nb3=2;
 if((i+1)<=M && i>=1 && j>=1 && j<=N)
     nb3=0;
-    if(matCellule(i+1,j)==1)
+    if(matCellule(i,j+1)==1)
         nb3=1;
     end
 end
 nb4=2;
 if((j+1)<=N && i>=1 && j>=1 && i<=M)
     nb4=0;
-    if(matCellule(i,j+1)==1)
+    if(matCellule(i+1,j)==1)
         nb4=1;
     end
 end
