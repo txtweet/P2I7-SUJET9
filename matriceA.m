3dbe70cf1c4abaa3a1e9fbeea4a7995a2c1c6e95
GaetanDenis <49226984+GaetanDenis@users.noreply.github.com>
2019-05-13 15:31:38 +0200

    ajout conditions en haut

--- matriceA.m
+++ matriceA.m
@@ -1,11 +1,13 @@
 function A=matriceA(noeudsHor,noeudsVert,matCellule)
-global B hc dx Tchauf lambdaair
+global B hc dx Tchauf lambdaair hcmurs
 A=zeros(noeudsHor*noeudsVert, noeudsHor*noeudsVert);
 %A est une matrice de taille noeudsHor*noeudsVert+3. On rajoute 3 points
 %correspondant a l'isolant de resistance tres elevee au sous-dalle
 %(indice 1), l'air au-dessus du plancher (indice N-2) et les murs (indice
 %N)
 %A=-h²Laplacien de T
+%hc coefficient d'echange pour le sol
+%hcmurs coefficient d'echange pour les murs
 
 for i=3:noeudsHor-3
     for j=3:noeudsVert-3
@@ -60,7 +62,7 @@ for i=3:noeudsHor-3
     %Condition aux limites periodiques 
     
     %Partie gauche de la cellule
-    for j=2:noeudsVert-1
+    for j=3:noeudsVert-3
         A(j,j)=4;
         A(j,(j+1))=-1;
         A(j,(j-1))=-1;
@@ -69,7 +71,7 @@ for i=3:noeudsHor-3
     end 
     
     %Partie droite de la cellule
-    for j=2:noeudsVert-1
+    for j=3:noeudsVert-3
         A((noeudsHor-1)*noeudsVert+j,(noeudsHor-1)*noeudsVert+j)=4;
         A((noeudsHor-1)*noeudsVert+j,(noeudsHor-1)*noeudsVert+(j+1))=-1;
         A((noeudsHor-1)*noeudsVert+j,(noeudsHor-1)*noeudsVert+(j-1))=-1;
@@ -89,6 +91,30 @@ for i=3:noeudsHor-3
     j=noeudsVert-1;
     for i=3:noeudsHor-3
         
+        
+    end
+    
+    %Conditions en haut du plancher
+    for i=1:noeudsHor
+        %au niveau du haut du plancher (j=N-2)
+        j=noeudsVert-2
+        k1=noeudsHor*(i-1)+j;
+        A(k1,k1)=hc;
+        A(k1,k1-1)=-lambdaair/(2*dx);
+        A(k1,k1+1)=-hc+lambdaair/(2*dx);
+        
+        %au niveau de l'air (j=N-1)
+        j=noeudsVert-1;
+        k2=noeudsHor*(i-1)+j;
+        A(k2,k2)=hc+hcmurs;
+        A(k2,k2-1)=-lambdaair/(2*dx)-hc;
+        A(k2,k2+1)=lambdaair/(2*dx)-hcmurs;
+        
+        %au niveau des murs (j=N)
+        j=noeudsVert;
+        k3=noeudsHor*(i-1)+j;
+        A(k3,k3)=lambdaair/dx+hcmurs;
+        A(k3,k3-1)=-lambdaair/dx-hcmurs;
         
     end
  
