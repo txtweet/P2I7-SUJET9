10a89838d60a290ac0d2ca0dae6a384bcd675b99
Vincent Beatrix <vincent.beatrix.9@gmail.com>
2019-05-15 08:32:00 +0200

    Commentaires

--- CreationGeometrieV2.m
+++ CreationGeometrieV2.m
@@ -6,8 +6,8 @@ global position_centre rayonConduiteNum matCellule matT B dx hc Tchauf lambdaair
 resolution = 1;                                         % nombre de noeuds par centimètre
 hcmurs=1e3;                                             % coefficient d'echanges convectifs des murs de la piece
 lambdaair=0.0262;                                       % conductivite thermique de l'air
-hauteurDalle = 8;                                      % hauteur de la dalle (en cm)
-largeurDalle = 8;                                      % largeur de la dalle (en cm)
+hauteurDalle = 8;                                       % hauteur de la dalle (en cm)
+largeurDalle = 8;                                       % largeur de la dalle (en cm)
 rayonConduite = 1;                                      % rayon de l'élément chauffant 
 hauteurConduite = (hauteurDalle+1)/2;                   % position du centre de l'élément chauffant par rapport au bas de la cellule modélisée
 masse = 500;                                            % masse de la dalle de béton
@@ -17,10 +17,10 @@ lambda = 0.92;                                          % conductivité thermique
 c_p = 2.5e6;                                            % capacité thermique du béton
 hc=1e3;                                                 % coefficient d'échanges convectifs de l'eau
 Tchauf=293;                                             % température de l'eau, constante (en K)
-dt=1;
-l=1; 
+dt=1;                                                   % pas de temps (discrétisation du temps)
+l=1;                                                    % discrétisation de l'espace
 Tdepart=30;                                             % temperature de la piece
-tmax=3;                                               % temps maximal de la simulation, en secondes
+tmax=3;                                                 % temps maximal de la simulation, en secondes
 %% Initialisation des paramètres
 noeudsVert= resolution * hauteurDalle;              % nombre de neuds sur la hauteur de la cellule
 noeudsHor = resolution * largeurDalle;              % nombre de neuds sur la largeur de la cellule
@@ -32,13 +32,13 @@ matT = zeros(noeudsVert,noeudsHor);                 % matrice dont chaque coeffi
 position_centre = floor([(noeudsHor+1)/2 resolution*hauteurConduite]);  % coordonnées du centre du tube contenant le fluide caloporteur
 % ATTENTION : pour une faible résolution et des dimensions de la dalle
 % paires, la position du centre est décentrée vers le haut à gauche.
-GenereMatrice();                                                  % matCellule et matT sont remplies selon la géométrie du problème
+GenereMatrice();    % matCellule et matT sont remplies selon la géométrie du problème
 %% Résolution du problème
-Tancien=zeros(noeudsHor*noeudsVert,1);                % matrice colonne qui contient la température en chaque point de chaque cellule
-Tneuf=ones(noeudsHor*noeudsVert,1);                % matrice colonne qui contient la température en chaque point de chaque cellule
-Tneuf(:)=Tdepart;                               %a changer
-B=zeros(noeudsHor*noeudsVert,1);                % matrice colonne
-A=matriceA(noeudsHor,noeudsVert,matCellule,Tneuf);    %
+Tancien=zeros(noeudsHor*noeudsVert,1);              % matrice colonne qui contient la température en chaque point de chaque cellule
+Tneuf=ones(noeudsHor*noeudsVert,1);                 % matrice colonne qui contient la température en chaque point de chaque cellule
+Tneuf(:)=Tdepart;                                   % a changer
+B=zeros(noeudsHor*noeudsVert,1);                    % matrice colonne
+A=matriceA(noeudsHor,noeudsVert,matCellule,Tneuf);  %
 i=dt;
 while i<tmax
     Tancien=Tneuf;
