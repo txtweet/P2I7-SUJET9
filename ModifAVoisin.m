f6a7e236b890fb3decbdf817ed6d880e7382f817
txtweet <39955857+txtweet@users.noreply.github.com>
2019-05-22 09:31:02 +0200

    vv

--- ModifAVoisin.m
+++ ModifAVoisin.m
@@ -2,44 +2,59 @@
 function A=ModifAVoisin(A, Voisin, i, j,Tavant)
 global dx l rho c_p noeudsVert hc dt Tchauf B lambda
     coord =(i-1)*noeudsVert+j;                  %Index du noeud considéré dans la matrice A et B
-    %ksolide=-lambda*dx*dx*dt*l/(rho*c_p*2*dx);     %Coefficient multiplicateur pour la derivée du solide
-    %kfluide=dx*l*hc/(lambda);               %Coefficient multiplicateur pour la derivée dans le fluide
+    ksolide=-lambda*dx*l*dt/(rho*c_p*dx*dx*l*2*dx);     %Coefficient multiplicateur pour la derivée du solide
+    kfluide=dx*dt*l*hc/(rho*c_p*dx*dx*l);               %Coefficient multiplicateur pour la derivée dans le fluide
     
     %% Voisins solide
-    A((i-1)*noeudsVert+j,(i-1)*noeudsVert+j)=4;
-    if (Voisin(1,1)==0) %gauche
-        A((i-1)*noeudsVert+j,(i-2)*noeudsVert+j)=-1;
+    if (Voisin(1,1)==0) %Voisin de droite
+        A(coord,coord)= A(coord,coord)-3/(2*dx);
+        A(coord,coord+noeudsVert) = 4/(2*dx);
+        A(coord,coord+2*noeudsVert) = -1/(2*dx);         
     end
-    if (Voisin(1,2)==0) %haut
-        A((i-1)*noeudsVert+j,(i-1)*noeudsVert+(j+1))=-1;
-    end
-    if (Voisin(1,3)==0) %bas
-        A((i-1)*noeudsVert+j,(i-1)*noeudsVert+(j-1))=-1;
-    end
-    if(Voisin(1,4)==0) %droite
-        A((i-1)*noeudsVert+j,(i)*noeudsVert+j)=-1;
-    end
-    k=-lambda*dt/(rho*c_p*dx*dx);
-    A((i-1)*noeudsVert+j,:)=A((i-1)*noeudsVert+j,:)*k;
-    A((i-1)*noeudsVert+j,(i-1)*noeudsVert+j)=A((i-1)*noeudsVert+j,(i-1)*noeudsVert+j)+1;
-    B((i-1)*noeudsVert+j,1)=Tavant((i-1)*noeudsVert+j,1);
-    %% Voisins fluide
-    %k=(-lambda*dt)/(rho*c_p*dx*dx);
-    if (Voisin(1,1)==1) %gauche
-        A(coord,coord)= A(coord,coord)-(hc/2-lambda/dx)*k/(-lambda/dx-hc/2);
-        B(coord,1) = B(coord,1)+(Tchauf*k)/(-lambda/dx-hc/2);
-    end
-    if (Voisin(1,2)==1) %haut
-        A(coord,coord)= A(coord,coord)+(hc/2-lambda/dx)*k/(-lambda/dx-hc/2);
-        B(coord,1) = B(coord,1)+(Tchauf*k)/(-lambda/dx-hc/2);
+    
+    if (Voisin(1,2)==0)
+        %Voisin du haut
+        A(coord,coord)= A(coord,coord)+3/(2*dx);
+        A(coord,coord-1) = -4/(2*dx);
+        A(coord,coord-2) = 1/(2*dx);
     end 
-    if (Voisin(1,3)==1) %bas
-        A(coord,coord)= A(coord,coord)-(hc/2-lambda/dx)*k/(-lambda/dx-hc/2);
-        B(coord,1) = B(coord,1)+(Tchauf*k)/(-lambda/dx-hc/2);
+    if (Voisin(1,3)==0)     
+        %Voisin du bas 
+        A(coord,coord)= A(coord,coord)-3/(2*dx);
+        A(coord,coord+1) = 4/(2*dx);
+        A(coord,coord+2) = -1/(2*dx);
     end
-    if (Voisin(1,4)==1) %droite
-        A(coord,coord)= A(coord,coord)+(hc/2-lambda/dx)*k/(-lambda/dx-hc/2);
-        B(coord,1) = B(coord,1)+(Tchauf*k)/(-lambda/dx-hc/2);
+    if (Voisin(1,4)==0)     
+        %Voisin de gauche
+        A(coord,coord)= A(coord,coord)+3/(2*dx);
+        A(coord,coord-noeudsVert) = -4/(2*dx);
+        A(coord,coord-2*noeudsVert) = 1/(2*dx);
+    end
+    A(coord,:)=ksolide.*A(coord,:);
+    A(coord,coord)=A(coord,coord)-1; %??
+    B(coord,1)=-Tavant(coord,1);
+    %% Voisins fluide
+    kfluide = -hc*l*dx*dt/(rho*c_p*dx*dx*l);
+    if (Voisin(1,1)==1)
+        A(coord,coord)= A(coord,coord)+kfluide*(-3/2);
+        A(coord,coord+noeudsVert) = A(coord,coord+noeudsVert)+kfluide*1/2;
+        B(coord,1) = B(coord,1)-kfluide*Tchauf;
+    end
+    
+    if (Voisin(1,2)==1)
+        A(coord,coord)= A(coord,coord)+kfluide*(-3/2);
+        A(coord,coord+1) = A(coord,coord+1)+kfluide*1/2;
+        B(coord,1) = B(coord,1)-kfluide*Tchauf;
+    end 
+    if (Voisin(1,3)==1)
+        A(coord,coord)= A(coord,coord)+kfluide*(-3/2);
+        A(coord,coord-1) = A(coord,coord-1)+kfluide*1/2;
+        B(coord,1) = B(coord,1)-kfluide*Tchauf;
+    end
+    if (Voisin(1,4)==1)
+        A(coord,coord)= A(coord,coord)+kfluide*(-3/2);
+        A(coord,coord-noeudsVert) = A(coord,coord-noeudsVert)+kfluide*1/2;
+        B(coord,1) = B(coord,1)-kfluide*Tchauf;
     end
 
 end
