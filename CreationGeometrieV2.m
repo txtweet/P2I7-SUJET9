1a5937c0ae07e709777db8e9d105c32deef1f43b
Vincent Beatrix <vincent.beatrix.9@gmail.com>
2019-05-22 10:40:27 +0200

    Ajout de paramètres

--- CreationGeometrieV2.m
+++ CreationGeometrieV2.m
@@ -4,11 +4,14 @@ clear all
 global position_centre rayonConduiteNum matCellule matT B dx hc Tchauf lambdaair hcmurs lambda rho c_p dt l noeudsVert noeudsHor lambdaisolant Tsol
 %% Variables du probleme
 resolution=5;                                           % nombre de noeuds par centimètre
-hcmurs=1e3;                                             % coefficient d'echanges convectifs des murs de la piece
+dt=1;                                                   % pas de temps (discrétisation du temps)
+l=1;                                                    % discrétisation de l'espace
 lambdaair=0.0262;                                       % conductivite thermique de l'air (d'après Cours de thermique, C. Obrecht)
 lambdaisolant=0.04;                                     % conductivite thermique de l'isolant du bas
 lambdamurs=0.04;                                        % conductivité thermique de l'isolant des murs
 lambdaeau=0.6;                                          % conductivité thermique de l'eau (d'après le site ekopedia.fr)
+lambda=0.142;                                           % conductivité thermique du béton (d'après le site laterlite.fr)
+lambda_sol=0.4;                                         % conductivité thermique du sol, ici gravier sec (d'après le site energieplus-lesite.be)
 hauteurDalle=10;                                        % hauteur de la dalle (en cm)
 largeurDalle=10;                                        % largeur de la dalle (en cm)
 rayonConduite=1;                                        % rayon de l'élément chauffant 
@@ -19,8 +22,9 @@ vitesse=debit/(pi*(rayonConduite*10^(-2)));             % vitesse de déplacement
 mu=0.653e-3;                                            % viscosité dynamique de l'eau (cours de thermique, C. Obrecht)
 rho_eau=1000;                                           % masse volumique de l'eau, untié S.I.
 rho=600;                                                % masse volumique du béton en kg.m^(-3) (d'après le site laterlite.fr)
+rho_isolant=20;                                         % masse volumique de l'isolant en kg.m^(-3) pour le polystyrène expansé (Guide des matériaux isolants, CAUE de la Haute-Loire)
+rho_murs=20;                                            % masse volumique des murs
 masse=rho*volume;                                       % masse de la dalle de béton
-lambda=0.142;                                           % conductivité thermique du béton (d'après le site laterlite.fr)
 c_p=1e3;                                                % capacité thermique massique du béton (d'apès le site laterlite.fr)
 c_p_eau=4178;                                           % capacité thermique massique de l'eau (cours de thermique, C. Obrecht)
 c_p_isolant=1400;                                       % capacité thermique massique de l'isolant (polystyrène expansé)
@@ -28,9 +32,8 @@ c_p_murs=1400;                                          % capacité thermique mas
 Re=(rho_eau*vitesse*(2*rayonConduite*10^(-2)))/mu;      % constante de Reynolds (cours de thermique, C. Obrecht)
 Pr=mu*c_p_eau/lambdaeau;                                % constante de Prandtl (cours de thermique, C. Obrecht)
 hc=(0.023*Re^(4/5)*Pr^(1/3))/(2*rayonConduite*10^(-2)); % coefficient d'échanges convectifs de l'eau
+hcmurs=1e3;                                             % coefficient d'echanges convectifs des murs de la piece
 Tchauf=40+273.15;                                       % température de l'eau, constante (en K)
-dt=1;                                                   % pas de temps (discrétisation du temps)
-l=1;                                                    % discrétisation de l'espace
 Tdepart=15+273.15;                                      % temperature de la piece
 tmax=10;                                                % temps maximal de la simulation, en secondes
 Text=3+273.15;                                          % temperature exterieure constante
