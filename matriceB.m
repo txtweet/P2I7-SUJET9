b19082a86a194b128b2500df0277014e01e60503
GaetanDenis <49226984+GaetanDenis@users.noreply.github.com>
2019-05-29 10:57:54 +0200

    changes

--- matriceB.m
+++ matriceB.m
@@ -1,10 +1,10 @@
 function B=matriceB(noeudsHor,noeudsVert,matCellule, Tavant, Text)
-global  hc dx Tchauf lambdaair hcmurs lambda rho c_p dt Tsol lambdaisolant l lambdasol rhomurs c_p_murs
+global  hc dx Tchauf lambdaair hcmurs lambda rho c_p dt Tsol lambdaisolant l lambdasol rhomurs c_p_murs rhoisolant c_p_isolant hcairmurs
 B=zeros(noeudsHor*noeudsVert, 1);
 
 for i=2:noeudsHor-1
-    for j=2:noeudsVert-3
-    %for j=3:noeudsVert-3
+    %for j=2:noeudsVert-3
+    for j=3:noeudsVert-3
         %cas ou la case etudiee est solide
         if (matCellule(j,i)==0)
             Voisins = DonneVoisins(i,j,matCellule);
@@ -40,42 +40,48 @@ end
     %Condition aux limites periodiques 
     
     %Partie gauche de la cellule
-    for j=2:noeudsVert-3
-    %for j=3:noeudsVert-3
+    %for j=2:noeudsVert-3
+    for j=3:noeudsVert-3
         B(index(1,j),1)=Tavant(index(1,j),1);
     end 
     %Partie droite de la cellule
-    for j=2:noeudsVert-3
-    %for j=3:noeudsVert-3
+    %for j=2:noeudsVert-3
+    for j=3:noeudsVert-3
         B(index(noeudsHor,j),1)=Tavant(index(noeudsHor,j),1);
     end
     
     %% Conditions en haut en la dalle
-    %au point le plus haut de la dalle
-    j=noeudsVert-2;
-    k1=noeudsVert*(i-1)+j;
-    B(k1)=Tavant(k1);
-    
-    %au noeud d'air
-    j=noeudsVert-1;
-    k2=noeudsVert*(i-1)+j;
-    B(k2)=Tavant(k2);
-    
-    %au noeud de murs
-    j=noeudsVert;
-    k3=noeudsVert*(i-1)+j;
-    B(k3)=Tavant(k3)+Text*lambda*dt/(rhomurs*c_p_murs*dx.^2)+Text*(hcmurs+lambdaair/dx);
-    if i>1 && i<noeudsHor
-        B(k3)=B(k3)+Tavant(k3);
-    end
-    
-    %% Conditions en bas de la dalle
-    j=1;
-    k4=noeudsVert*(i-1)+j;
-    B(k4)=B(k4)-Tsol*2*(lambdaisolant+lambdasol)/(rho*c_p*dx^2);
-    %if i~=1 && i~=noeudsHor
-        B(k4)=B(k4)+Tavant(k4);
-    %end
-    B(index(i,2))=Tavant(index(i,2));
+    for i=1:noeudsHor
+        %au point le plus haut de la dalle
+        j=noeudsVert-2;
+        %k1=noeudsVert*(i-1)+j;
+        B(index(i,j))=Tavant(index(i,j));
+
+        %au noeud d'air
+        j=noeudsVert-1;
+        %k2=noeudsVert*(i-1)+j;
+        B(index(i,j))=Tavant(index(i,j));
+
+        %au noeud de murs
+        j=noeudsVert;
+        %k3=noeudsVert*(i-1)+j;
+        %B(index(i,j))=Tavant(index(i,j))+Text*lambda*dt/(rhomurs*c_p_murs*dx.^2)+Text*(hcmurs+lambdaair/dx);
+        B(index(i,j))=Tavant(index(i,j))+Text*hcairmurs*dt/(rhomurs*c_p_murs*dx^2);
+%         if i>1 && i<noeudsHor
+%             B(k3)=B(k3)+Tavant(k3);
+%         end
+
+        %% Conditions en bas de la dalle
+        j=1;
+        B(index(i,j))=Tsol*(lambdasol+lambdaisolant)*dt/(rhoisolant*c_p_isolant*dx^2)+Tavant(index(i,j));
+        
+        j=2;
+        B(index(i,j))=Tavant(index(i,j));
+    %     k4=noeudsVert*(i-1)+j;
+    %     B(k4)=B(k4)-Tsol*2*(lambdaisolant+lambdasol)/(rho*c_p*dx^2);
+    %     %if i~=1 && i~=noeudsHor
+    %         B(k4)=B(k4)+Tavant(k4);
+    %     %end
+    %     B(index(i,2))=Tavant(index(i,2));
 
 end
