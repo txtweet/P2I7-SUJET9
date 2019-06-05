0f9b36591c3c4701052d7e8beb08262c966bde00
Vincent Beatrix <vincent.beatrix.9@gmail.com>
2019-06-05 10:23:13 +0200

    Modif commentaires CreationGeometrieV2

--- CreationGeometrieV2.m
+++ CreationGeometrieV2.m
@@ -4,14 +4,14 @@ clear all
 global position_centre rayonConduiteNum matCellule matT B dx hc hcairdalle hcairmurs Tchauf lambdaair hcmurs lambda rho c_p dt l noeudsVert noeudsHor lambdaisolant Tsol Text lambdamurs lambdasol c_p_air c_p_murs rhomurs rhoair c_p_isolant rhoisolant eisolant esol
 %% Variables du probleme
 resolution=3;                                           % nombre de noeuds par centimètre
-dt=30;                                                   % pas de temps (discrétisation du temps)
+dt=30;                                                  % pas de temps (discrétisation du temps)
 l=1;                                                    % discrétisation de l'espace
 lambdaair=0.0262;                                       % conductivite thermique de l'air (d'après Cours de thermique, C. Obrecht)
-lambdaisolant=0.038;                                     % conductivite thermique de l'isolant du bas
+lambdaisolant=0.038;                                    % conductivite thermique de l'isolant du bas
 lambdamurs=0.05;                                        % conductivité thermique de l'isolant des murs
 lambdaeau=0.6;                                          % conductivité thermique de l'eau (d'après C. OBRECHT)
-lambda=300;                                             % conductivité thermique du béton (d'après C. OBRECHT)
-lambdasol=0.04;                                          % conductivité thermique du sol, ici gravier sec (d'après le site energieplus-lesite.be)
+lambda=300;                                             % conductivité thermique du matériau de la dalle (ici : béton, d'après C. OBRECHT)
+lambdasol=0.04;                                         % conductivité thermique du sol, ici gravier sec (d'après le site energieplus-lesite.be)
 hauteurDalle=10;                                        % hauteur de la dalle (en cm)
 largeurDalle=10;                                        % largeur de la dalle (en cm)
 rayonConduite=1;                                        % rayon de l'élément chauffant 
@@ -21,31 +21,31 @@ debit=100e-3/3600;                                      % débit du fluide calopo
 vitesse=debit/(pi*(rayonConduite*10^(-2)));             % vitesse de déplacement du fluide caloporteur dans le tube
 mu=0.653e-3;                                            % viscosité dynamique de l'eau (cours de thermique, C. Obrecht)
 rho_eau=1000;                                           % masse volumique de l'eau, untié S.I.
-rho=2400;                                                % masse volumique du béton en kg.m^(-3) (d'après le site ciment.wikibis.com)
+rho=2400;                                               % masse volumique du matériau de la dalle en kg.m^(-3) (ici : béton, d'après le site ciment.wikibis.com)
 rhoisolant=25;                                          % masse volumique de l'isolant en kg.m^(-3) pour le polystyrène expansé (Guide des matériaux isolants, CAUE de la Haute-Loire)
-rhomurs=2000;                                             % masse volumique des murs
+rhomurs=2000;                                           % masse volumique des murs
 rhoair=1.225;                                           % masse volumique de l'air
-masse=rho*volume;                                       % masse de la dalle de béton
-c_p=1e3;                                                % capacité thermique massique du béton (d'apès le site laterlite.fr)
+masse=rho*volume;                                       % masse de la dalle
+c_p=1e3;                                                % capacité thermique massique du matériau de la dalle (ici : béton, d'apès le site laterlite.fr)
 c_p_eau=4178;                                           % capacité thermique massique de l'eau (cours de thermique, C. Obrecht)
 c_p_isolant=1000;                                       % capacité thermique massique de l'isolant (polystyrène expansé)
 c_p_murs=1000;                                          % capacité thermique massique des murs
 c_p_air=1004;                                           % capacité thermique massique de l'air
-eisolant=100e-2;                                         % epaisseur de la couche d'isolant sous dalle, en metres
-esol=1;                                                 % epaisseur de la couche de sol sous isolant, en metres (tres importante pour grande isolation)
+eisolant=100e-2;                                        % épaisseur de la couche d'isolant sous dalle, en mètres
+esol=1;                                                 % épaisseur de la couche de sol sous isolant, en mètres (tres importante pour grande isolation)
 Re=(rho_eau*vitesse*(2*rayonConduite*10^(-2)))/mu;      % constante de Reynolds (cours de thermique, C. Obrecht)
 Pr=mu*c_p_eau/lambdaeau;                                % constante de Prandtl (cours de thermique, C. Obrecht)
 hc=(0.023*Re^(4/5)*Pr^(1/3))/(2*rayonConduite*10^(-2)); % coefficient d'échanges convectifs de l'eau
 hcmurs=1e3;                                             % coefficient d'echanges convectifs des murs de la piece
-hcairdalle=10;                                          % coefficient d'échanges convectifs entre l'air et la dalle **PAS SUR**
+hcairdalle=10;                                          % coefficient d'échanges convectifs entre l'air et la dalle (pour le béton ici)
 hcairmurs=10;                                           % coefficient d'échanges convectifs entre l'air intérieur et les murs
-Tchauf=500+273.15;                                       % température de l'eau, constante (en K)
-Tdepart=15+273.15;                                      % temperature de la piece
-tmaxheures=15;                                           % temps maximal de la simulation, en heures
-%tmax=tmaxheures*3600;                                   % temps maximal de la simulation, en secondes
+Tchauf=500+273.15;                                      % température de l'eau, constante (en K)
+Tdepart=15+273.15;                                      % température de la pièce
+tmaxheures=15;                                          % temps maximal de la simulation, en heures
+%tmax=tmaxheures*3600;                                  % temps maximal de la simulation, en secondes
 tmax=20*dt;
-Text=0+273.15;                                          % temperature exterieure constante
-Tsol=0+273.15;                                        % temperature du sol (sous la dalle)
+Text=0+273.15;                                          % température extérieure constante
+Tsol=0+273.15;                                          % température du sol (sous la dalle)
 %% Initialisation des paramètres
 noeudsVert=resolution * hauteurDalle;               % nombre de neuds sur la hauteur de la cellule
 noeudsHor =resolution * largeurDalle;               % nombre de neuds sur la largeur de la cellule
@@ -83,6 +83,6 @@ view(2);
 % Mise en evidence du noeud d'air
 figure();
 Tair=zeros(size(T(noeudsVert-1,:)));
-Tair(:)=T(noeudsVert-1,:)
+Tair(:)=T(noeudsVert-1,:);
 plot(Tair);
 %%
