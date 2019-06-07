a8f1d2dd1906c714579aaa9d6cc4c8629d2c1b23
txtweet <39955857+txtweet@users.noreply.github.com>
2019-06-07 16:10:52 +0200

    ipd

--- GenereMatriceCarree.m
+++ GenereMatriceCarree.m
@@ -1,13 +1,14 @@
 %%%%%%%%%%%%%%%%%%%% P2I7 - SUJET 9 %%%%%%%%%%%%%%%%%%%%
-%%% Remplissage d'une matrice carrée pour le fluide %%%
+%%% Remplissage d'une matrice carrée pour le fluide %%%%
+%%%          (utilisé pour le debuggage)            %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function GenereMatriceCarree()
 global position_centre rayonConduiteNum matCellule matT Tchauf
 % Les coefficients de la matrice matCelulle prennent la valeur 0 s'il
 % s'agit de béton, 1 s'il s'agit du fluide
 for i=position_centre(1)-rayonConduiteNum:position_centre(1)+rayonConduiteNum
     for j=position_centre(2)-rayonConduiteNum:position_centre(2)+rayonConduiteNum
-            matCellule(i,j)=1;
-           matT(i,j)=Tchauf;
+            matCellule(i,j)=1;  %Mise à jour du masque
     end
 end
 end
